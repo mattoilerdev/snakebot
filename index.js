7f53290676ce33cd2273a9c02457eeb880e7ed3c
mattoilerdev <oliosvuotato@gmail.com>
2023-05-09 12:18:07 +0200

    anticrash 2

--- index.js
+++ index.js
@@ -1,56 +1,34 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: 3276799 });
+const Discord = require("discord.js")
+const client = new Discord.Client({ intents: 3276799 })
+const express = require('express')
+const app =  express()
 const config = require('./config.json')
-const chalk = require("chalk")
+const chalk = import("chalk")
 const prefix = "!" 
 const guildsnake = ("960106570451128320")
 
 app.listen(3000, () => {
-    console.log(
-      chalk.white('['),
-      chalk.cyan('Express'),
-      chalk.white(']'),
-      chalk.gray(':'),
-      chalk.white('Connected')
-    );
+    console.log('Anticrash Funzionante');
   });
   
 
 // ———————————————[Error Handling]———————————————
 process.on("unhandledRejection", (reason, p) => {
-    console.log(chalk.gray("—————————————————————————————————"));
-    console.log(
-      chalk.white("["),
-      chalk.red.bold("AntiCrash"),
-      chalk.white("]"),
-      chalk.gray(" : "),
-      chalk.white.bold("Unhandled Rejection/Catch")
-    );
-    console.log(chalk.gray("—————————————————————————————————"));
+    console.log("—————————————————————————————————");
+    console.log("Anticrash, unhandled rejection")
+    console.log("—————————————————————————————————");
     console.log(reason, p);
   });
   process.on("uncaughtException", (err, origin) => {
-    console.log(chalk.gray("—————————————————————————————————"));
-    console.log(
-      chalk.white("["),
-      chalk.red.bold("AntiCrash"),
-      chalk.white("]"),
-      chalk.gray(" : "),
-      chalk.white.bold("Uncaught Exception/Catch")
-    );
-    console.log(chalk.gray("—————————————————————————————————"));
+    console.log("—————————————————————————————————");
+    console.log("Anticrash, uncaught Expression")
+    console.log("—————————————————————————————————");
     console.log(err, origin);
   });
   process.on("multipleResolves", (type, promise, reason) => {
-    console.log(chalk.gray("—————————————————————————————————"));
-    console.log(
-      chalk.white("["),
-      chalk.red.bold("AntiCrash"),
-      chalk.white("]"),
-      chalk.gray(" : "),
-      chalk.white.bold("Multiple Resolves")
-    );
-    console.log(chalk.gray("—————————————————————————————————"));
+    console.log("—————————————————————————————————");
+    console.logchalk("Anticrash, Multiple Resolves")
+    console.log("—————————————————————————————————");
     console.log(type, promise, reason);
   });
   
@@ -1762,4 +1740,4 @@ distube.on("searchNoResult", (message, query) => {
 
 
 ///////////////////////////////////////////////////////////////////////////////
-client.login(config.token) 
+client.login('MTAxOTIxNTM3NDg0MTAzNjgwMQ.Go32is.YnOzEojtBrnq7Oz_0MfQ322nvk6eniB_EOZEs0') 
